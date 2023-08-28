f74c5552f77079aa57fe04eeee31b1d0267c9c3a
Andrei N. Onea <andrei@onea.ro>
2023-08-28 21:11:45 +0300

    tema2: added collision checks for track limits and other cars via sphere

--- src/lab_m1/Tema2/Tema2.cpp
+++ src/lab_m1/Tema2/Tema2.cpp
@@ -20,25 +20,29 @@ using namespace m1;
  *  and the order in which they are called, see `world.cpp`.
  */
 
+#define CPU_CAR(index) "cpu" + std::to_string(index)
+#define next_float() (float) (rand()) / (float) RAND_MAX
+
 static vector<glm::vec3> trees_pos{};
 static vector<glm::vec3> track_pois{};
 static vector<std::pair<glm::vec3, glm::vec3>> track_segments{};
-static float camera_offset_xz{};
-static float camera_offset_y{};
-static glm::vec3 player_pos{};
-static glm::vec3 player_dir{};
-static float player_speed{};
-static float player_turn_speed{};
-static float initial_player_rotation{};
-static float frontwheel_angle{};
-static bool piua{};
-static bool wireframe{};
-static float track_width{};
-static unsigned int start_line_idx{};
+static float camera_offset_xz{ 1.0f };
+static float camera_offset_y{ 0.5f };
+static glm::vec3 player_pos{ glm::vec3{ -16.5f, 0.0f, 1.0f } };
+static glm::vec3 player_dir = glm::vec3_left;
+static float player_speed{ 4.9f };
+static float player_turn_speed{ 90.0f };
+static float initial_player_rotation{ -90.0f };
+static float frontwheel_angle{ 0.0f };
+static bool piua{ true };
+static bool wireframe{ false };
+static float track_width{ 2.0f };
+static float half_track_width = track_width * 0.5f;
+static unsigned int start_line_idx{ 14 };
+static unsigned int player_curr_segment = start_line_idx;
+static float car_width = 0.20f;
+static float car_length = 0.50f;
 
-#define CPU_CAR(index) "cpu" + std::to_string(index)
-
-#define next_float() (float) (rand()) / (float) RAND_MAX
 
 namespace m1
 {
@@ -78,18 +82,18 @@ namespace m1
             float random = next_float();
 
             // The direction of the next track segment -- usually perpendicular to the direction of travel
-            glm::vec3 segment_dir = track_segments[next_point].first - track_segments[next_point].second;
+            glm::vec3 segment_dir = glm::normalize(track_segments[next_point].first - track_segments[next_point].second);
 
             // Choose to offset either left or right for more variation
             if (random < 0.5f)
                 segment_dir *= -1.0f;
 
-            // After using the random for direction choice, extend it to track width length.
-            // Now we have random value in [0, track_width]
-            random *= track_width;
+            // After using the random for direction choice, extend it to track width length, but restrict to maximum quarter deviation.
+            // Now we have random value in [0, track_width / 4]
+            random *= track_width * 0.25f;
 
-            // Find next path to follow by going [0, track_width / 4] to the left or right from the centered point
-            target = track_pois[next_point] + (segment_dir * (random * 0.25f));
+            // Find next path to follow by going randomly to the left or right from the centered line
+            target = track_pois[next_point] + (segment_dir * random);
             dir = glm::normalize(target - pos);
         }
     };
@@ -138,23 +142,14 @@ Tema2::~Tema2()
 {
 }
 
+bool CheckSpheresCollision(const glm::vec3& o1, float r1, const glm::vec3& o2, float r2)
+{
+    return glm::length(o2 - o1) < r1 + r2;
+}
 
 void Tema2::Init()
 {
     srand(time(nullptr));
-
-    camera_offset_xz = 1.0f;
-    camera_offset_y = 0.5f;
-    player_pos = glm::vec3{ -16.5f, 0.0f, 1.0f };
-    player_dir = glm::vec3{ -1.0f, 0.0f, 0.0f };
-    player_speed = 2.9f;
-    player_turn_speed = 45.0f;
-    initial_player_rotation = -90.0f;
-    frontwheel_angle = 0.0f;
-    piua = true;
-    wireframe = false;
-    track_width = 1.0f;
-    start_line_idx = 14;
 
     // Load and generate track
     {
@@ -177,11 +172,11 @@ void Tema2::Init()
             glm::vec3 side_dir = glm::normalize(glm::cross(segment, glm::vec3_up));
 
             // i = interior, e = exterior
-            glm::vec3 i1 = p1 + track_width * side_dir;
-            glm::vec3 e1 = p1 - track_width * side_dir;
+            glm::vec3 i1 = p1 + half_track_width * side_dir;
+            glm::vec3 e1 = p1 - half_track_width * side_dir;
 
-            glm::vec3 i2 = p2 + track_width * side_dir;
-            glm::vec3 e2 = p2 - track_width * side_dir;
+            glm::vec3 i2 = p2 + half_track_width * side_dir;
+            glm::vec3 e2 = p2 - half_track_width * side_dir;
 
             track_pois.push_back(p1);
             track_pois.push_back(p2);
@@ -217,13 +212,13 @@ void Tema2::Init()
         meshes["track"]->SetDrawMode(GL_TRIANGLE_STRIP);
 
         // Create start/finish line somewhere
-        glm::vec3 istart = track_segments[start_line_idx].first;
-        glm::vec3 estart = track_segments[start_line_idx].second;
-        glm::vec3 inext = track_segments[start_line_idx - 1].first;
-        glm::vec3 enext = track_segments[start_line_idx - 1].second;
-        glm::vec3 dir = glm::normalize(inext - istart);
-        glm::vec3 iend = istart + 0.2f * dir;
-        glm::vec3 eend = estart + 0.2f * dir;
+        const glm::vec3& istart = track_segments[start_line_idx].first;
+        const glm::vec3& estart = track_segments[start_line_idx].second;
+        const glm::vec3& inext = track_segments[start_line_idx - 1].first;
+        const glm::vec3& enext = track_segments[start_line_idx - 1].second;
+        const glm::vec3& dir = glm::normalize(inext - istart);
+        const glm::vec3& iend = istart + 0.2f * dir;
+        const glm::vec3& eend = estart + 0.2f * dir;
 
         vector<VertexFormat> sf_vertices
         {
@@ -240,9 +235,9 @@ void Tema2::Init()
     }
 
     // Create CPU-controlled car
-    AddCpu(player_pos + glm::vec3_forward * 0.5f, 10, 2.9f, glm::vec3{ 0.671f, 0.324f, 0.245f }); // left side from player
-    AddCpu(player_pos - glm::vec3_left * 1.0f, 10, 2.6f, glm::vec3{ 0.724f, 0.871f, 0.745f }); // one row behind
-    AddCpu(player_pos + glm::vec3_forward * 0.5f - glm::vec3_left * 1.0f, 10, 3.0f, glm::vec3{ 0.824f, 0.871f, 0.145f }); // left side from player, one row behind
+    AddCpu(player_pos + glm::vec3{ -0.25f, 0.0f, 0.5f }, 10, 4.9f, glm::vec3{ 0.671f, 0.324f, 0.245f }); // left side from player
+    AddCpu(player_pos - glm::vec3_left * 1.0f, 10, 4.6f, glm::vec3{ 0.724f, 0.871f, 0.745f }); // one row behind
+    AddCpu(player_pos + glm::vec3{ 0.75f, 0.0f, 0.5f }, 10, 5.0f, glm::vec3{ 0.824f, 0.871f, 0.145f }); // left side from player, one row behind
 
     // Create player-controlled car
     {
@@ -409,25 +404,54 @@ void Tema2::FrameEnd()
  */
 
 
+float ProjectPointLine_RH(glm::vec3 &projection, const glm::vec3& p, const glm::vec3& l1, const glm::vec3& l2)
+{
+    float length = glm::length(l2 - l1);
+    float u = (
+        (p.x - l1.x) * (l2.x - l1.x)
+        + (p.y - l1.y) * (l2.y - l1.y)
+        + (p.z - l1.z) * (l2.z - l1.z)
+        ) / (length * length);
+    projection.x = l1.x + u * (l2.x - l1.x);
+    projection.y = l1.y + u * (l2.y - l1.y);
+    projection.z = l1.z + u * (l2.z - l1.z);
+    return u;
+}
+
+
 void Tema2::OnInputUpdate(float deltaTime, int mods)
 {
     if (window->MouseHold(GLFW_MOUSE_BUTTON_2) || piua)
         return;
 
+    for (const Cpu& cpu : cpu_cars)
+        if (CheckSpheresCollision(player_pos, std::fmaxf(car_length, car_width), cpu.pos, std::fmaxf(car_length, car_width)))
+            return;
+
+    float actual_speed = player_speed;
+    float actual_turn_speed = player_turn_speed;
+    glm::vec3 new_player_pos = player_pos;
+
+    if (mods & GLFW_MOD_SHIFT)
+    {
+        actual_speed *= 2;
+        actual_turn_speed *= 2;
+    }
+
     if (window->KeyHold(GLFW_KEY_W))
-        player_pos += player_dir * player_speed * deltaTime;
+        new_player_pos += player_dir * actual_speed * deltaTime;
     if (window->KeyHold(GLFW_KEY_S))
-        player_pos -= player_dir * player_speed * deltaTime;
+        new_player_pos -= player_dir * actual_speed * deltaTime;
     if (window->KeyHold(GLFW_KEY_A))
     {
-        float left_turn = player_turn_speed * deltaTime;
+        float left_turn = actual_turn_speed * deltaTime;
         frontwheel_angle += left_turn;
         if (frontwheel_angle > 45.0f && false)
             frontwheel_angle = 45.0f;
     }
     if (window->KeyHold(GLFW_KEY_D))
     {
-        float right_turn = -player_turn_speed * deltaTime;
+        float right_turn = -actual_turn_speed * deltaTime;
         frontwheel_angle += right_turn;
         if (frontwheel_angle < -45.0f && false)
             frontwheel_angle = -45.0f;
@@ -436,9 +460,40 @@ void Tema2::OnInputUpdate(float deltaTime, int mods)
     float angle_rad = RADIANS(initial_player_rotation + frontwheel_angle);
     player_dir = glm::vec3{ glm::sinf(angle_rad), 0.0f, glm::cosf(angle_rad) };
 
-    glm::vec3 camera_pos = player_pos + (-player_dir * camera_offset_xz) + (glm::vec3_up * camera_offset_y);
+    glm::vec3 camera_pos = new_player_pos + (-player_dir * camera_offset_xz) + (glm::vec3_up * camera_offset_y);
     glm::quat camera_rotation = glm::quatLookAt(player_dir, glm::vec3_up);
     GetSceneCamera()->SetPositionAndRotation(camera_pos, camera_rotation);
+
+    glm::vec3 point_on_line{};
+    float offset_in_line = ProjectPointLine_RH(point_on_line, new_player_pos, track_pois[(player_curr_segment + 1) % track_pois.size()], track_pois[player_curr_segment]);
+
+    std::cout << "Offset from segment " << player_curr_segment << ": " << offset_in_line << '\n';
+
+    if (offset_in_line < 0.0f)
+    {
+        player_curr_segment = (player_curr_segment + 1) % track_pois.size();
+        float next_segment = (player_curr_segment + 1) % track_pois.size();
+        std::cout << "Player went backward to segment " << player_curr_segment << '\n';
+        std::cout << "                   next segment " << next_segment << '\n';
+        ProjectPointLine_RH(point_on_line, new_player_pos, track_pois[player_curr_segment], track_pois[next_segment]);
+    }
+    else if (offset_in_line > 1.0f)
+    {
+        float previous_segment = player_curr_segment;
+        player_curr_segment = (player_curr_segment == 0) ? track_pois.size() - 1 : player_curr_segment - 1;
+        std::cout << "Player went forward to segment " << player_curr_segment << '\n';
+        std::cout << "                   old segment " << previous_segment<< '\n';
+        ProjectPointLine_RH(point_on_line, new_player_pos, track_pois[previous_segment], track_pois[player_curr_segment]);
+    }
+    std::cout << "Player in segment " << player_curr_segment << '\n';
+
+    const float dist = glm::length(new_player_pos - point_on_line);
+    if (dist > half_track_width)
+    {
+        std::cout << "Player out of track bounds! Distance = " << dist << '\n';
+    }
+    else
+        player_pos = new_player_pos;
 }
 
 
